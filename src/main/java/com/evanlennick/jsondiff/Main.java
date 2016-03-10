c3c834e76ea0ab1cc2782a9ef8fb6cae84dbf0d5
Evan Lennick <evan.lennick@accolade.com>
2016-03-10 16:32:09 -0500

    fat jar with dependencies will get generated now during the package phase... also cleaned up output

--- src/main/java/com/evanlennick/jsondiff/Main.java
+++ src/main/java/com/evanlennick/jsondiff/Main.java
@@ -2,36 +2,48 @@ package com.evanlennick.jsondiff;
 
 import com.fasterxml.jackson.databind.JsonNode;
 import com.fasterxml.jackson.databind.ObjectMapper;
+import com.fasterxml.jackson.databind.SerializationFeature;
 import com.flipkart.zjsonpatch.JsonDiff;
-import org.apache.commons.io.IOUtils;
 
 import java.io.IOException;
-import java.io.InputStream;
+import java.nio.file.Files;
+import java.nio.file.Paths;
 
 public class Main {
 
     public static void main(String[] args) throws IOException {
-        String json1 = getFileContents("aim-dev.json");
-        String json2 = getFileContents("aim-dev-d0.json");
+        if (args.length != 2) {
+            throw new IllegalArgumentException("Requires two arguments!");
+        }
+
+        String path1 = args[0];
+        String path2 = args[1];
+
+        String json1 = getFileContents(path1);
+        String json2 = getFileContents(path2);
+
+        System.out.println("\n-----------------------------------------------------");
+        System.out.println("Diff when going from " + path1 + " to " + path2 + "\n");
         printDiff(json1, json2);
+
+        System.out.println("\n-----------------------------------------------------");
+        System.out.println("Diff when going from " + path2 + " to " + path1 + "\n");
         printDiff(json2, json1);
     }
 
     public static void printDiff(String json1, String json2) throws IOException {
-        ObjectMapper objectMapper = new ObjectMapper();
+        ObjectMapper mapper = new ObjectMapper();
+        mapper.enable(SerializationFeature.INDENT_OUTPUT);
 
-        JsonNode from = objectMapper.readTree(json1);
-        JsonNode to = objectMapper.readTree(json2);
+        JsonNode from = mapper.readTree(json1);
+        JsonNode to = mapper.readTree(json2);
         JsonNode patch = JsonDiff.asJson(from, to);
 
-        System.out.println(patch);
+        System.out.println(mapper.writerWithDefaultPrettyPrinter().writeValueAsString(patch));
     }
 
-    public static String getFileContents(String filename) throws IOException {
-        InputStream resourceAsStream = Main.class.getClassLoader().getResourceAsStream(filename);
-        String contents = IOUtils.toString(resourceAsStream, "UTF-8");
-        IOUtils.closeQuietly(resourceAsStream);
-        return contents;
+    public static String getFileContents(String path) throws IOException {
+        return new String(Files.readAllBytes(Paths.get(path)));
     }
 
 }
